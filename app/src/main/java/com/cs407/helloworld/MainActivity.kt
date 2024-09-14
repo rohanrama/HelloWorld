13f47a3ea3690e21d412a16c5ec902d9277de6e6
Rohan Ramachandran <rnramachandr@wisc.edu>
2024-09-14 14:15:46 -0500

    An Image View Variable was added to the MainActivity class

--- app/src/main/java/com/cs407/helloworld/MainActivity.kt
+++ app/src/main/java/com/cs407/helloworld/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageView
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var ingView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
